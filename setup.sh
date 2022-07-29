889b62361ed2bf7928c591963554a2970a6fc72b
Cody Peterson <cody.dkdc2@gmail.com>
2022-07-29 18:48:35 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-# clone repositories
-gh repo clone dbt-labs/dbt-core
-gh repo clone dbt-labs/dbt-snowflake
-gh repo clone dbt-labs/dbt-redshift
-gh repo clone dbt-labs/dbt-bigquery
-gh repo clone dbt-labs/dbt-spark
-gh repo clone dbt-labs/dbt-databricks
-gh repo clone dbt-labs/dbt-server
-gh repo clone dbt-labs/dbt-utils
+# installs
+brew install tree cloc
+
+# clone repositories - I never learned for loops
+
+## Core stuff
+gh repo clone $org/dbt-core
+gh repo clone $org/dbt-snowflake
+gh repo clone $org/dbt-redshift
+gh repo clone $org/dbt-bigquery
+gh repo clone $org/dbt-spark
+gh repo clone $org/dbt-databricks
+
+## "Core" stuff
+gh repo clone $org/actions
+gh repo clone $org/dbt-rpc
+gh repo clone $org/dbt-server
+gh repo clone $org/dbt-utils
+gh repo clone $org/dbt-starter-project
+
+## Examples
+gh repo clone $org/jaffle_shop
+gh repo clone $org/jaffle_shop_duckdb
+
+## Other
+gh repo clone $org/internal-analytics
+gh repo clone $org/docs.getdbt.com
+
+## it's fine
+exit 0
