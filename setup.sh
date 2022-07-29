9743352037a1546401e20f0877fa0d7e60e82e09
lostmygithubaccount <cody.dkdc2@gmail.com>
2022-07-28 17:35:18 -0700

    update

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# clone repositories
+gh repo clone dbt-labs/dbt-core
+gh repo clone dbt-labs/dbt-snowflake
+gh repo clone dbt-labs/dbt-redshift
+gh repo clone dbt-labs/dbt-bigquery
+gh repo clone dbt-labs/dbt-spark
+gh repo clone dbt-labs/dbt-databricks
+gh repo clone dbt-labs/dbt-server
+gh repo clone dbt-labs/dbt-utils
