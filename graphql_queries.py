beb7c6030e6cc8e96a901e177debdf955dc56a10
Cody Peterson <cody.dkdc2@gmail.com>
2022-08-05 04:30:42 +0000

    updates

--- graphql_queries.py
+++ graphql_queries.py
@@ -25,7 +25,7 @@ issues_query = """
                                 }
                             }
                         }
-                        participants(first:100) {
+                        participants(first:20) {
                             edges {
                                 node {
                                     login
@@ -45,6 +45,54 @@ issues_query = """
     }
 """
 
+# graphql query to get list of prs for the github repo
+prs_query = """
+    query {
+        repository(owner:$org, name:$repo) {
+            pullRequests(last:$num_items states:OPEN filterBy: {
+                labels: [$label]
+            }) {
+                edges {
+                    node {
+                        title
+                        assignees(first:10) {
+                            edges {
+                                node {
+                                    login
+                                }
+                            }
+                        }
+                        number
+                        id
+                        url
+                        labels(first:10) {
+                            edges {
+                                node {
+                                    name
+                                }
+                            }
+                        }
+                        participants(first:20) {
+                            edges {
+                                node {
+                                    login
+                                }
+                            }
+                        }
+                        createdAt
+                        updatedAt
+                        closedAt
+                        author {
+                            login
+                        }
+                    }
+                }
+            }
+        }
+    }
+"""
+
+# graphql query to get project id by its number (and org)
 project_id_query = """
     query {
         organization(login: $org) {
@@ -55,6 +103,7 @@ project_id_query = """
     }
 """
 
+# graphql mutation to add item to project (v2) by project_id and content_id
 add_item_to_project_mutation = """
     mutation {
         addProjectV2ItemById(input: {projectId: $project_id contentId: $item_id}) {
