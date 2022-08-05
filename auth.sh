e010a14df4f6649c46403c51049134c93d140426
Cody Peterson <cody.dkdc2@gmail.com>
2022-08-05 05:45:37 +0000

    qp

--- auth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-unset GITHUB_TOKEN
-echo $CODESPACE_PAT | gh auth login --with-token
-gh auth setup-git
-git config --global user.email "cody.dkdc2@gmail.com"
-git config --global user.name "Cody Peterson"
