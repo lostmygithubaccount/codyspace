2a26561bb8a4e04e564a8edd086e12dac347af19
Cody Peterson <cody.dkdc2@gmail.com>
2022-08-12 01:39:46 +0000

    gcloud install in installs.sh

--- installs.sh
+++ installs.sh
@@ -8,4 +8,7 @@ pip install --upgrade pip # yolo
 pip install -r requirements.txt
 
 # gcp installs
-apt-get install apt-transport-https ca-certificates gnupg
+sudo apt-get install apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
