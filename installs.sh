df73e9813c3d198a27750522e736137c8d3b97e5
Cody Peterson <cody.dkdc2@gmail.com>
2022-09-16 20:27:48 +0000

    minor edit

--- installs.sh
+++ installs.sh
@@ -3,9 +3,12 @@
 # brew installs
 brew install tree cloc
 
+# linux
+apt-get update && apt-get upgrade -y
+
+# wild
+apt-get install libsasl2-dev
+
 # python installs
 pip install --upgrade pip # yolo
 pip install -r requirements.txt
-
-# linux
-apt-get update && apt-get upgrade -y
